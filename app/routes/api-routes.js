e1ae40d4d0b7bafd4a0c2ee7513cd508f40054c6
Armand Guim <armandguim3@gmail.com>
2018-02-03 06:56:17 -0500

    basic routing

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,1 +1,9 @@
-test lol
+var path = require('path');
+
+var tableData = require('../data/table-data.js');
+
+var waitinglistData = require('../data/waitinglist-data.js');
+
+module.exports = function(app) {
+
+}
